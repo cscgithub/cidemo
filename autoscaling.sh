96f10c4e23cc46d8829de5b2d0e1dfba07cc6d6f
Daniel Brain <github@biboop.com>
2013-02-05 09:48:01 +1100

    Add tag to autoscaling group

--- autoscaling.sh
+++ autoscaling.sh
@@ -1,5 +1,5 @@
 as-create-launch-config CIDemoLaunch --region ap-southeast-2 --image-id ami-2e7fe914 --instance-type t1.micro --key lukeaaus --group cidemo
-as-create-auto-scaling-group CIDemoASG --region ap-southeast-2 --launch-configuration CIDemoLaunch --availability-zones ap-southeast-2b --min-size 1 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb
+as-create-auto-scaling-group CIDemoASG --region ap-southeast-2 --launch-configuration CIDemoLaunch --availability-zones ap-southeast-2b --min-size 1 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb --tag "k=name, v=cidemo, p=true"
 export SCALE_UP_ARN=`as-put-scaling-policy CIDemoScaleUp --region ap-southeast-2 --auto-scaling-group CIDemoASG --adjustment=1 --type ChangeInCapacity --cooldown 60`
 mon-put-metric-alarm CIDemoHighCPU --region ap-southeast-2 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 70 --alarm-actions $SCALE_UP_ARN --dimensions "AutoScalingGroupName=CIDemoASG"
 export SCALE_DOWN_ARN=`as-put-scaling-policy CIDemoScaleDown --region ap-southeast-2 --auto-scaling-group CIDemoASG --adjustment=-1 --type ChangeInCapacity  --cooldown 60`
