dc5579a3192f6b521f1aaf2dcc312e0545172c45
Daniel Brain <github@biboop.com>
2013-02-05 11:28:28 +1100

    Create image from instance and use for autoscaling config

--- autoscaling.sh
+++ autoscaling.sh
@@ -1,9 +1,9 @@
-as-create-launch-config CIDemoLaunch --region ap-southeast-2 --image-id ami-2e7fe914 --instance-type t1.micro --key lukeaaus --group cidemo
-as-create-auto-scaling-group CIDemoASG --region ap-southeast-2 --launch-configuration CIDemoLaunch --availability-zones ap-southeast-2b --min-size 1 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb --tag "k=name, v=cidemo, p=true"
-export SCALE_UP_ARN=`as-put-scaling-policy CIDemoScaleUp --region ap-southeast-2 --auto-scaling-group CIDemoASG --adjustment=1 --type ChangeInCapacity --cooldown 60`
-mon-put-metric-alarm CIDemoHighCPU --region ap-southeast-2 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 70 --alarm-actions $SCALE_UP_ARN --dimensions "AutoScalingGroupName=CIDemoASG"
-export SCALE_DOWN_ARN=`as-put-scaling-policy CIDemoScaleDown --region ap-southeast-2 --auto-scaling-group CIDemoASG --adjustment=-1 --type ChangeInCapacity  --cooldown 60`
-mon-put-metric-alarm CIDemoLowCPU --region ap-southeast-2 --comparison-operator LessThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 40 --alarm-actions $SCALE_DOWN_ARN --dimensions "AutoScalingGroupName=CIDemoASG"
+export INSTANCE_ID=`ec2-describe-instances --region ap-southeast-2 --filter "tag-key=aws:cloudformation:stack-name" --filter "tag-value=$1" | awk '/INSTANCE/{print $2}'`
+export IMAGE_ID=`ec2-create-image --region ap-southeast-2 -n $1 -d "Image for build $1" i-328cbf08 | awk '/IMAGE/{print $2}'`
 
-# Set key name
-# Set instance name
+as-create-launch-config $1-lc --region ap-southeast-2 --image-id $IMAGE_ID --instance-type t1.micro --key lukeaaus --group cidemo
+as-create-auto-scaling-group $1-asg --region ap-southeast-2 --launch-configuration $1-lc --availability-zones ap-southeast-2b --min-size 1 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb --tag "k=Name, v=$1, p=true"
+export SCALE_UP_ARN=`as-put-scaling-policy $1-ScaleUp --region ap-southeast-2 --auto-scaling-group $1-asg --adjustment=1 --type ChangeInCapacity --cooldown 60`
+mon-put-metric-alarm $1-HighCPU --region ap-southeast-2 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 70 --alarm-actions $SCALE_UP_ARN --dimensions "AutoScalingGroupName=$1-asg"
+export SCALE_DOWN_ARN=`as-put-scaling-policy $1-ScaleDown --region ap-southeast-2 --auto-scaling-group $1-asg --adjustment=-1 --type ChangeInCapacity --cooldown 60`
+mon-put-metric-alarm $1-LowCPU --region ap-southeast-2 --comparison-operator LessThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 40 --alarm-actions $SCALE_DOWN_ARN --dimensions "AutoScalingGroupName=$1-asg"
