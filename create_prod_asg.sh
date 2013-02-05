35858dadd4d580a37b44f4a6d14f5c720a0ea316
Daniel Brain <github@biboop.com>
2013-02-06 10:24:48 +1100

    Show build number on home page

--- create_prod_asg.sh
+++ create_prod_asg.sh
@@ -1,4 +1,4 @@
-as-create-auto-scaling-group cidemo-asg --region ap-southeast-2 --launch-configuration cidemo-27-lc --availability-zones ap-southeast-2b --min-size 1 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb --tag "k=Name, v=cidemo-as, p=true"
+as-create-auto-scaling-group cidemo-asg --region ap-southeast-2 --launch-configuration cidemo-27-lc --availability-zones ap-southeast-2b --min-size 2 --max-size 10 --desired-capacity 1 --load-balancers cidemo-lb --tag "k=Name, v=cidemo-as, p=true"
 export SCALE_UP_ARN=`as-put-scaling-policy cidemo-ScaleUp --region ap-southeast-2 --auto-scaling-group cidemo-asg --adjustment=1 --type ChangeInCapacity --cooldown 60`
 mon-put-metric-alarm cidemo-HighCPU --region ap-southeast-2 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --metric-name CPUUtilization --namespace "AWS/EC2" --period 60 --statistic Average --threshold 70 --alarm-actions $SCALE_UP_ARN --dimensions "AutoScalingGroupName=cidemo-asg"
 export SCALE_DOWN_ARN=`as-put-scaling-policy cidemo-ScaleDown --region ap-southeast-2 --auto-scaling-group cidemo-asg --adjustment=-1 --type ChangeInCapacity --cooldown 60`
--- src/main/java/springmvc/demo/controller/HomeController.java
+++ src/main/java/springmvc/demo/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Properties;
+import java.io.InputStream;
 
 /**
  * Sample controller for going to the home page with a message 
@@ -14,8 +16,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private final Properties buildProperties;
+
+	public HomeController() {
+		buildProperties = new Properties();
+		try {
+			final InputStream propInputStream = getClass().getClassLoader().getResourceAsStream("build.properties");
+			if (propInputStream != null) {
+				buildProperties.load(propInputStream);
+			}
+		} catch (Exception e) {
+			logger.error("Failed to load build.properties", e);
+		}
+	}
 
 	/**
 	 * Selects the home page and populates the model with a message
@@ -23,8 +37,9 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 		logger.info("Welcome home!");
-		model.addAttribute("controllerMessage",
-				"This is the message from the controller!");
+
+		model.addAttribute("buildNumber", "cidemo-" + buildProperties.getProperty("buildNumber", "##"));
+		model.addAttribute("controllerMessage", "This is the message from the controller!");
 		return "home";
 	}
 	
