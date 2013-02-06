37d0c87f79c4dcd762e3534503dbb2b638d94bba
Daniel Brain <github@biboop.com>
2013-02-06 16:01:00 +1100

    Dont honor cooldown when manually executing policies

--- autoscale.sh
+++ autoscale.sh
@@ -17,4 +17,4 @@ export IMAGE_ID=`ec2-create-image --region ap-southeast-2 -n $1 -d "Image for bu
 as-create-launch-config $1-lc --region ap-southeast-2 --image-id $IMAGE_ID --instance-type t1.micro --key lukeaaus --group cidemo
 as-update-auto-scaling-group cidemo-test-asg --region ap-southeast-2 --launch-configuration $1-lc
 as-describe-auto-scaling-groups cidemo-test-asg --region ap-southeast-2 | awk '/INSTANCE/{print $2}' | while read instance; do ec2-terminate-instances $instance --region ap-southeast-2; done
-as-execute-policy cidemo-test-ScaleUp --auto-scaling-group cidemo-test-asg --region ap-southeast-2
+as-execute-policy cidemo-test-ScaleUp --auto-scaling-group cidemo-test-asg --region ap-southeast-2 --no-honor-cooldown
