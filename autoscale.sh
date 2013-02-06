e97a215eb14f65888bc43e03396afe115123bde8
Daniel Brain <github@biboop.com>
2013-02-06 17:31:00 +1100

    Fix script for build box

--- autoscale.sh
+++ autoscale.sh
@@ -16,15 +16,15 @@ export IMAGE_ID=`ec2-create-image --region ap-southeast-2 -n $1 -d "Image for bu
 export IMAGE_AVAILABLE="false"
 COUNTER=0
 while [ "$IMAGE_AVAILABLE" != "true" ]; do
-	if [ "$COUNTER" -gt 24 ]; then
-		echo "Quitting after 25 attempts"
-		break
-	else
-		sleep 5s
-		echo "Checking for image availability"
-		IMAGE_AVAILABLE=`ec2-describe-images $IMAGE_ID --region ap-southeast-2 | awk '/BLOCKDEVICEMAPPING/{print $6}'`
-		let COUNTER=COUNTER+1
-	fi
+    if [[ "$COUNTER" -gt 24 ]]; then
+        echo "Quitting after 25 attempts"
+        break
+    else
+        sleep 5s
+        echo "Checking for image availability"
+        IMAGE_AVAILABLE=`ec2-describe-images $IMAGE_ID --region ap-southeast-2 | awk '/BLOCKDEVICEMAPPING/{print $5}'`
+        let COUNTER=COUNTER+1
+    fi
 done
 
 as-create-launch-config $1-lc --region ap-southeast-2 --image-id $IMAGE_ID --instance-type t1.micro --key lukeaaus --group cidemo
