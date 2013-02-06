cb9e151bc9ce8dc90a43f282daaab04421e41d9d
Daniel Brain <github@biboop.com>
2013-02-06 16:38:53 +1100

    Wait for image to become available before breaking test

--- autoscale.sh
+++ autoscale.sh
@@ -13,8 +13,14 @@ while [ -z "$INSTANCE_ID" ]; do
 done
 
 export IMAGE_ID=`ec2-create-image --region ap-southeast-2 -n $1 -d "Image for build $1" $INSTANCE_ID | awk '/IMAGE/{print $2}'`
+export IMAGE_AVAILABLE="false"
+while [ "$IMAGE_AVAILABLE" -ne "true" ]; do
+	sleep 5s
+	echo "Waiting for image to become available"
+	IMAGE_AVAILABLE=`ec2-describe-images $IMAGE_ID --region ap-southeast-2 | awk '/BLOCKDEVICEMAPPING/{print $6}'`
+done
 
 as-create-launch-config $1-lc --region ap-southeast-2 --image-id $IMAGE_ID --instance-type t1.micro --key lukeaaus --group cidemo
 as-update-auto-scaling-group cidemo-test-asg --region ap-southeast-2 --launch-configuration $1-lc
-as-describe-auto-scaling-groups cidemo-test-asg --region ap-southeast-2 | awk '/INSTANCE/{print $2}' | while read instance; do ec2-terminate-instances $instance --region ap-southeast-2; done
 as-execute-policy cidemo-test-ScaleUp --auto-scaling-group cidemo-test-asg --region ap-southeast-2 --no-honor-cooldown
+as-describe-auto-scaling-groups cidemo-test-asg --region ap-southeast-2 | awk '/INSTANCE/{print $2}' | while read instance; do ec2-terminate-instances $instance --region ap-southeast-2; done
