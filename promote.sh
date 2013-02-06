33abc336f68545a5c90aa6b2c2e9d21de4e44662
Daniel Brain <github@biboop.com>
2013-02-06 11:03:00 +1100

    Use the autoscaling policies to commission new versions

--- promote.sh
+++ promote.sh
@@ -1,1 +1,2 @@
 as-update-auto-scaling-group cidemo-asg --region ap-southeast-2 --launch-configuration $1-lc
+as-execute-policy cidemo-ScaleUp --auto-scaling-group cidemo-asg --region ap-southeast-2
